4d9de2d12214436bf4fdcfbcfcb9ee36b55f6977
bostjanpisler <bostjan@studio404.net>
2018-02-10 17:41:30 +0100

    First

---
+++ src/extension.ts
@@ -0,0 +1,21 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerCommand('debug-arg.prompt', promptForArg));
+}
+
+export function deactivate() {
+}
+
+async function promptForArg(context: vscode.ExtensionContext): Promise<string> {
+    let options: vscode.InputBoxOptions = {
+        prompt: "Argument: ",
+        placeHolder: ""
+    }
+    
+    return vscode.window.showInputBox(options)
+        .then(value => {
+            return value;
+        });
+}
